e6f1a533e1ae3a935794039e35b138482934d32e
Christian Hagen Wiker <chrishawik@gmail.com>
2021-10-18 15:43:54 +0200

    FIN (NO COMMENTS)

--- Compulsory2/DECLARATIONS.h
+++ Compulsory2/DECLARATIONS.h
@@ -1,1 +1,40 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <cstdlib>
+#include <ctime>
+#include <Windows.h>
+#include <vector>
+
+void login();
+void game();
+void playerCard();
+void dealerCard();
+void printSum();
+void printHand();
+int playerSum();
+int dealerSum();
+
+bool userResult{};
+bool passResult{};
+
+int playerCurrency = 100;
+std::vector <int> playerHand;
+bool playerTurn = true;
+bool playerWin = false;
+
+
+int dealerCurrency = 100;
+std::vector <int> dealerHand;
+bool dealerTurn = false;
+bool dealerWin = false;
+
+char aceInput{};
+char input{};
+int count{};
+int betInput{};
+int betWin{};
+
+bool stopPlaying = false;
--- Compulsory2/Source.cpp
+++ Compulsory2/Source.cpp
@@ -1,22 +1,10 @@
-#include <iostream>
-#include <string>
-#include <time.h>
-#include <cstdlib>
-#include <ctime>
-
-void login();
-void game();
-void playerCard();
-void dealerCard();
-void printSum();
-void printHand();
-
-bool userResult{};
-bool passResult{};
+#include "declarations.h"
 
 void login()
 {
-		
+	int failCount{};
+	while (failCount < 5) 
+	{
 		const std::string username = "admin";
 		const std::string password = "password";
 		char user_array[5]{};
@@ -70,230 +58,276 @@ void login()
 			}
 		}
 
+
+		if (userResult == true && passResult == true) {
+			return;
+		}
+		else {
+		failCount++;
+		}
+	}
+	std::cout << "Too many failed attempts, closing" << std::endl;
+	exit(0);
 }
 
-int playerCurrency = 100;
-int playerHand[5];
-int playerSum;
-int playerHandSize;
-bool playerTurn = true;
-bool playerWin = false;
 
-
-int dealerCurrency = 100;
-int dealerHand[5];
-int dealerSum;
-int dealerHandSize;
-bool dealerTurn = false;
-bool dealerWin = false;
-
-char input{};
 
 
 void playerCard()
 {
-	srand(time(0));
-
-	if (playerHandSize <= 5)
-	{
-		playerHand[playerHandSize] = 1 + (rand() % 11);
-		playerHandSize++;
-	}
-
-	else
-	{
-		std::cout << "You have the maximum amount of cards" << std::endl;
-	}
-
-
+		playerHand.push_back(1 + (rand() % 10));
 }
 
 void dealerCard()
 {
-	if (dealerHandSize <= 5)
-	{
-		dealerHand[dealerHandSize] = 1 + (rand() % 11);
-		dealerHandSize++;
-	}
-
+		dealerHand.push_back(1 + (rand() % 10));
 }
 
 void printSum()
-{
-	for (int i = 0; i < playerHandSize; i++)
-	{
-		playerSum += playerHand[i];
-	}
-
-	for (int j = 0; j < dealerHandSize; j++)
-	{
-		dealerSum += dealerHand[j];
-	}
-
-	std::cout << "Your current hand sum: " << playerSum << std::endl;
-
-	std::cout << "Dealer's hand sum: " << dealerSum << std::endl;
+{	
+	std::cout << "Your current hand sum: " << playerSum() << std::endl;
+	std::cout << "Dealer's hand sum: " << dealerSum() << std::endl;
 }
 
 void printHand()
 {
 	std::cout << "Your hand: ";
 
-	for (int i = 0; i < playerHandSize; i++)
+	for (int i{}; i < playerHand.size(); i++) 
 	{
-		std::cout << "|" << playerHand[i];
+		std::cout << playerHand[i] << " | ";
 	}
-
-	std::cout << "|";
-
 	std::cout << std::endl;
+
 
 	std::cout << "Dealer's hand: ";
 
-	for (int j = 0; j < dealerHandSize; j++)
+	
+	for (int i{}; i < dealerHand.size(); i++) 
 	{
-		std::cout << "|" << dealerHand[j];
+		std::cout << dealerHand[i] << " | ";
 	}
-
-	std::cout << "|";
-
 	std::cout << std::endl;
+	
 }
 
 void game()
 {
+	playerHand.clear();
+	dealerHand.clear();
 
-	/*playerSum = 0;
-	playerHandSize = 0;
+	stopPlaying = false;
 
-	dealerSum = 0;
-	dealerHandSize = 0;
-
+	playerTurn = true;
+	dealerTurn = false;
 
 	system("cls");
 	std::cout << "Welcome to Blackjack!\n";
-	system("pause");
+	
+	std::cout << "Your capital: " << playerCurrency << std::endl;
+	std::cout << "House capital: " << dealerCurrency << std::endl;
+	std::cout << "Enter bet amount: ";
+	std::cin >> betInput;
+	std::cin.ignore(10000, '\n');
+	std::cin.clear();
+
+	if (betInput > dealerCurrency || betInput > playerCurrency)
+	{
+		std::cout << "Invalid amount";
+		std::cout << std::endl;
+		system("pause");
+		return;
+	}
+
+	betWin = betInput * 2;
+
+	playerCurrency -= betInput;
+	dealerCurrency -= betInput;
+
 	system("cls");
 
-	playerCard();
+	//playerCard();
 	playerCard();
 
-	dealerCard();
+	//dealerCard();
 	dealerCard();
 
 	printHand();
-	printSum();*/
+	printSum();
+
+	if (stopPlaying == true)
+	{
+		game();
+	}
 
 	while (playerTurn == true)
 	{
-		playerSum = 0;
-		dealerSum = 0;
 
-		if (playerSum == 21)
+
+		if (playerSum() == 21)
 		{
 			std::cout << "Player has blackjack and wins the hand!";
+			Sleep(2000);
+			playerTurn = false;
+			playerCurrency += betWin;
+			stopPlaying = true;
 			break;
 
 		}
-		else if (playerSum < 21)
+		else if (playerSum() > 21)
 		{
-			std::cout << "H - hit  |  S - stand";
+			std::cout << "You bust! Dealer wins!";
+			Sleep(2000);
+			playerTurn = false;
+			dealerCurrency += betWin;
+			stopPlaying = true;
+			break;
+		}
+		else if (playerSum() < 21)
+		{
+			std::cout << "H - hit  |  S - stand" << std::endl;
 			std::cin >> input;
 			if (input == tolower('H'))
 			{
 				system("cls");
 				playerCard();
+				bool changed = false;
+				while (playerHand.back() == 1 && changed == false)
+				{
+					std::cout << "Make the ace 1 or 11?: ";
+					std::cin >> aceInput;
+
+					switch (aceInput)
+					{
+					case 1:
+						playerHand.back() = 1;
+						changed = true;
+						break;
+					case 11:
+						playerHand.back() = 11;
+						changed = true;
+						break;
+					default:
+						std::cout << "Invalid input" << std::endl;
+						break;
+					}
+				}
+				
 				printHand();
 				printSum();
-				
+				count++;
 			}
 			else if (input == tolower('S'))
 			{
 				system("cls");
 				playerTurn = false;
 				dealerTurn = true;
-		
-			}
-		}
 
-		
-
-
-		while (dealerTurn == true)
-		{
-
-			if (dealerSum == 21)
-			{
-				std::cout << "Dealer has blackjack and wins the hand!";
-				break;
 			}
-			else if (dealerSum < 17)
-			{
-				dealerCard();
-				printHand();
-				printSum();
-				dealerTurn = false;
-			
-			}
-			else if (dealerSum >= 17)
-			{
-				break;
-			}
+
 
 		}
 
-		if (playerSum > dealerSum)
+
+	}
+
+	while (dealerTurn == true)
+	{
+
+		if (dealerSum() == 21)
 		{
-			std::cout << "You win the hand!";
+			std::cout << "Dealer has blackjack and wins the hand!";
+			Sleep(2000);
+			dealerCurrency += betWin;
+			stopPlaying = true;
 			break;
 		}
-		else if (dealerSum > playerSum)
+		else if (dealerSum() > 21)
+		{
+			std::cout << "Dealer busts! You win!";
+			Sleep(2000);
+			playerCurrency += betWin;
+			stopPlaying = true;
+			break;
+		}
+		else if (dealerSum() < 17)
+		{
+			Sleep(1000);
+			system("cls");
+			dealerCard();
+			printHand();
+			printSum();
+			count++;
+				
+			
+		}
+		else if (dealerSum() >= 17)
+		{
+			dealerTurn = false;
+			//break;
+		}
+
+	}
+
+	while (playerTurn == false && dealerTurn == false && stopPlaying == false)
+	{
+
+		if (playerSum() > dealerSum())
+		{
+			std::cout << "You win the hand!";
+			Sleep(2000);
+			playerCurrency += betWin;
+			stopPlaying = true;
+			break;
+				
+		}
+		else if (dealerSum() > playerSum())
 		{
 			std::cout << "Dealer wins the hand!";
+			Sleep(2000);
+			dealerCurrency += betWin;
+			stopPlaying = true;
+			break;
+				
+		}
+		else if (playerSum() == dealerSum())
+		{
+			std::cout << "It's a draw!";
 			break;
 		}
 
 	}
+
+	
 }
 
 int main()
 {
 	srand(time(0));
-
-	while (userResult == false || passResult == false)
-	{
-		login();
-
-		if (userResult == true || passResult == true)
-		{
-			break;
-		}
-	}
-
-	playerSum = 0;
-	playerHandSize = 0;
-
-	dealerSum = 0;
-	dealerHandSize = 0;
-
-
-	system("cls");
-	std::cout << "Welcome to Blackjack!\n";
-	system("pause");
-	system("cls");
-
-	playerCard();
-	playerCard();
-
-	dealerCard();
-	dealerCard();
-
-	printHand();
-	printSum();
-	
-	while (playerWin == false || dealerWin == false)
+	login();
+	while (playerCurrency > 0 && dealerCurrency > 0)
 	{
 		game();
 	}
-	
+	return 0;
+}
+
+int playerSum() 
+{
+	int sum{};
+	for (int i{}; i < playerHand.size(); i++) 
+	{
+		sum += playerHand[i];
+	}
+	return sum;
+}
+
+int dealerSum()
+{
+	int sum{};
+	for (int i{}; i < dealerHand.size(); i++)
+	{
+		sum += dealerHand[i];
+	}
+	return sum;
 }
